7a0cc04e85838793a2ff6482b5286babb91aa8fd
andreiulinici <andrei.ulinici1@gmail.com>
2017-07-07 19:20:06 +0300

    Add finished project

---
+++ frontend/src/app/messages-component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { WebService } from './web.service';
+
+@Component({
+    selector: 'messages',
+    template: `
+        <div *ngFor="let message of webService.messages | async">
+            <md-card class="card">
+                <md-card-title [routerLink]="['/messages', message.owner]" style="cursor: pointer">{{message.owner}}</md-card-title>
+                <md-card-content>{{message.text}}</md-card-content>
+            </md-card>
+        </div>
+    `
+})
+export class MessagesComponent {
+
+    constructor(private webService: WebService, private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        var name = this.route.snapshot.params['name'];
+        this.webService.getMessages(name);
+        this.webService.getUser().subscribe();
+    }
+}
